9b024d856279454f9fef07ecd836fb8207f7dcfc
Vico08lv <vimor@P-MPL-CROCO.dhcp.mpl.intranet>
2024-03-15 15:30:45 +0100

    test commit link account

--- server/resolvers.js
+++ server/resolvers.js
@@ -7,14 +7,14 @@ export const resolvers = {
         jobs: () => getJobs(),
         job: async (_root, { id }) => {
             const job = await getJob(id)
-            if(!job) {
+            if (!job) {
                 throw notFoundError('No Job found with id ' + id)
             }
             return job
         },
-        company: async (_root, { id }) => { 
+        company: async (_root, { id }) => {
             const company = await getCompany(id)
-            if(!company) {
+            if (!company) {
                 throw notFoundError('No Company found with id ' + id)
             }
             return company
@@ -23,30 +23,30 @@ export const resolvers = {
 
     Mutation: {
         // title & description are destructured from the input
-        createJob: (_root, { input: { title, description }}, { user }) => {
-            if(!user) {
+        createJob: (_root, { input: { title, description } }, { user }) => {
+            if (!user) {
                 throw unauthorizedError('Missing authentication')
             }
             return createJob({ companyId: user.companyId, title, description })
         },
 
         deleteJob: async (_root, { id }, { user }) => {
-            if(!user) {
+            if (!user) {
                 throw unauthorizedError('Missing authentication')
             }
             const job = await deleteJob(id, user.companyId)
-            if(!job){
+            if (!job) {
                 throw notFoundError('No Job found with id ' + id)
             }
             return job
         },
 
-        updateJob: async (_root, {input: { id, title, description }}, { user }) => {
-            if(!user) {
+        updateJob: async (_root, { input: { id, title, description } }, { user }) => {
+            if (!user) {
                 throw unauthorizedError('Missing authentication')
             }
             const job = updateJob({ id, companyId: user.companyId, title, description })
-            if(!job) {
+            if (!job) {
                 throw notFoundError('No Job found with id ' + id)
             }
             return job
@@ -69,14 +69,14 @@ function toIsoDate(date) {
 }
 
 
-function notFoundError(message)  {
-    return new GraphQLError(message,{
-        extensions: { code: 'NOT_FOUND'}
+function notFoundError(message) {
+    return new GraphQLError(message, {
+        extensions: { code: 'NOT_FOUND' }
     })
 }
 
-function unauthorizedError(message)  {
-    return new GraphQLError(message,{
-        extensions: { code: 'UNAUTHORIZED'}
+function unauthorizedError(message) {
+    return new GraphQLError(message, {
+        extensions: { code: 'UNAUTHORIZED' }
     })
 }
